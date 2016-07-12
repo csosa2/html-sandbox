2df5ff2881e8bd264f4af4c422acadf53c2d14e9
Christina Sosa <csosa2106@gmail.com>
2016-07-12 15:09:15 -0600

    added comments

--- index.php
+++ index.php
@@ -5,23 +5,30 @@
 		<title>My Web Page</title>
 	</head>
 	<body>
+		<!--Begin Header-->
 		<header>
 			<h1>Hai World!</h1>
 		</header>
+		<!--Start Main Content Here -->
 		<main>
+			<!--id is unique and can only be used once-->
 			<p id="first-paragraph">Attack dog, run away and pretend to be victim lie on your belly and purr when you are asleep, yet spread kitty litter all over house and my left donut is missing, as is my right pee in the shoe fall over dead (not really but gets sypathy). Human give me <strong>attention</strong> meow curl into a furry donut refuse to drink water except out of someone's glass yet stares at human while pushing stuff off a table or hide from vacuum cleaner. Kitty ipsum dolor sit amet, shed everywhere shed everywhere stretching attack your ankles chase the red dot, hairball run catnip eat the grass sniff. Put butt in owner's face poop in the plant pot intently sniff hand, yet steal the warm chair right after you get up. Always hungry put toy mouse in food bowl run out of litter box at full speed and attack the dog then pretend like nothing happened. Eat prawns <em>daintily</em> with a claw then lick paws clean wash down prawns with a lap of carnation milk then retire to the warmest spot on the couch to claw at the fabric before taking a catnap leave fur on owners clothes eat grass, throw it back up poop on grasses.</p>
 			<h2 class="pretty-title red-text">About Me</h2>
+			<!--multiple classes can be added to an element; can be used with multiple elements-->
 			<p>My left donut is missing, as is my right lick the other cats eat owner's food attack dog, run away and pretend to be victim. Scratch at the door then walk away paw at beetle and eat it before it gets away so eat from dog's food or burrow under covers, and meow but jump around on couch, meow constantly until given food, , or lick arm hair. Brown cats with pink ears vommit food and eat it again poop on grasses drink water out of the faucet play time jump around on couch, meow constantly until given food, .</p>
+			<!--unordered list-->
 			<ul>
 				<li>item 1</li>
 				<li>item 2</li>
 				<li>item 3</li>
 			</ul>
+			<!--ordered list-->
 			<ol>
 				<li>item 1</li>
 				<li>item 2</li>
 				<li>item 3</li>
 			</ol>
+			<!--cartoon image-->
 			<div>
 				<p>Go to <a href="https://google.com" target="_blank">Google.com</a></p>
 			</div>
@@ -46,7 +53,7 @@
 			</table>
 		</main>
 		<footer>
-			&copy; 2016
+			&copy; 2016 <!--&copy is the copyright symbol-->
 		</footer>
 	</body>
 </html>
