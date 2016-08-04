6bf67b398c4cfc424130a703e8a5fc0b6c2a1017
Christina Sosa <csosa2106@gmail.com>
2016-08-04 08:53:33 -0600

    reset files for bootstrap

--- about-me/index.php
+++ about-me/index.php
@@ -1,7 +1,114 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: Sosa
- * Date: 7/28/16
- * Time: 10:23 AM
- */
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8"/ >
+		<!--adding google fonts-->
+		<link href='https://fonts.googleapis.com/css?family=Athiti:400,700,600,500' rel='stylesheet' type='text/css'>
+		<!--adding custom CSS-->
+		<link href="css/style.css" rel="stylesheet" type="text/css"/>
+
+		<!--adding custom JavaScript-->
+		<script src="js/custom.js" type="text/javascript"></script>
+
+		<title>All About Me</title>
+	</head>
+	<!--main body begins-->
+	<body>
+		<header class="main-header">
+			<div class="container">
+				<h1 class="name">
+					About Me: Christina Sosa
+				</h1>
+				<h2>
+					Welcome to my website!
+				</h2>
+			</div>
+		</header>
+		<main>
+			<div class="text-area">
+				<div>
+					<img src="images/pyramidTop.jpg" alt="Top of Pyramid">
+					<p>
+						Hello, my name is Christina. I'm originally from Brooklyn, New York but have lived in New Mexico for
+						many years now (thanks Intel!). Many people ask if I miss New York. In reality, I enjoy the open and
+						beautiful views that New Mexico has to offer. Plus the sunsets aren't too shabby. My family is
+						originally from Mexico and I love going there to visit, taste the amazing food and soak in the
+						beautiful culture. In my last trip, I got the opportunity to climb to the top of a pyramid!
+					</p>
+				</div>
+				<div>
+					<img src="images/watchShows.jpg" alt="Watching show with dog">
+					<p>
+						When not traveling, I like to spend time with my friends and family. I live with my amazing
+						grandmother and constantly get spoiled with her delightful Mexican cuisine. After a satisfying meal, I
+						enjoy watching	an episode of <em>The Walking Dead</em> with my dog, Jasmine. As you can see, she also
+						enjoys watching it with me. This was an intense episode!
+					</p>
+				</div>
+				<div>
+					<img src="images/award.jpg" alt="medal">
+					<p>
+						For fun, I like to powerlift. Here are some things I learned through the sport:
+					</p>
+					<ul>
+						<li>
+							Powerlifting is more of a mental challenge than it is a physical challenge.
+						</li>
+						<li>
+							It’s okay to fail. It’s okay to miss a lift or have an off day of training. I've learned more from
+							failure than I have from success. I can look back and see where I went wrong and how I need to
+							improve.
+						</li>
+						<li>
+							It's cool to keep up with the boys. It's even cooler to be stronger than them.
+						</li>
+					</ul>
+					<p>
+						Last year I did something crazy. Something that I would have never done were it not for my coaches.
+						I competed in my first ever powerlifting meet. I never thought in a million years I would stand in
+						front of a large crowd, wearing a singlet, and have people intentionally stare at my
+						butt to see if I "hit depth" on my squat. It was one of the most exhausting days of my life, but I
+						made it through. It was an incredible experience.
+					</p>
+				</div>
+				<!--Submission Form-->
+				<h3>
+					What's your favorite thing about New Mexico?
+				</h3>
+				<form method="post" id="contact">
+					<table>
+						<tr>
+							<td>
+								<label>
+									Name:
+								</label>
+								<br>
+								<input type="text" name="name1" required>
+							</td>
+						</tr>
+						<tr>
+							<td>
+								<label>
+									Your Message:
+								</label>
+								<br>
+								<textarea maxlength="500" name="message" required>
+										500 characters max.
+									</textarea>
+							</td>
+						</tr>
+						<tr>
+							<td>
+								<input type="submit" value="Submit">
+							</td>
+						</tr>
+					</table>
+				</form>
+		</main>
+		</div>
+		<!--footer with copyright-->
+		<footer class="main-footer">
+			&copy 2016
+		</footer>
+	</body>
+</html>
--- index.php
+++ index.php
@@ -1,68 +1,9 @@
 <!DOCTYPE html>
 <html lang="en">
 	<head>
-		<meta charset="utf-8"/>
 
-		<!--adding custom CSS-->
-		<link href="css/style.css" rel="stylesheet" type="text/css"/>
-
-		<!--adding custom JavaScript-->
-		<script src="js/custom.js" type="text/javascript"></script>
-
-		<title>My Web Page</title>
 	</head>
 	<body>
-		<!--Begin Header-->
-		<header>
-			<h1>Hai World!</h1>
-		</header>
-		<!--Start Main Content Here -->
-		<main>
-			<!--id is unique and can only be used once-->
-			<p id="first-paragraph">Attack dog, run away and pretend to be victim lie on your belly and purr when you are asleep, yet spread kitty litter all over house and my left donut is missing, as is my right pee in the shoe fall over dead (not really but gets sypathy). Human give me <strong>attention</strong> meow curl into a furry donut refuse to drink water except out of someone's glass yet stares at human while pushing stuff off a table or hide from vacuum cleaner. Kitty ipsum dolor sit amet, shed everywhere shed everywhere stretching attack your ankles chase the red dot, hairball run catnip eat the grass sniff. Put butt in owner's face poop in the plant pot intently sniff hand, yet steal the warm chair right after you get up. Always hungry put toy mouse in food bowl run out of litter box at full speed and attack the dog then pretend like nothing happened. Eat prawns <em>daintily</em> with a claw then lick paws clean wash down prawns with a lap of carnation milk then retire to the warmest spot on the couch to claw at the fabric before taking a catnap leave fur on owners clothes eat grass, throw it back up poop on grasses.</p>
-			<h2 class="pretty-title foo-title">About Me</h2>
-			<!--multiple classes can be added to an element; can be used with multiple elements-->
-			<p>My left donut is missing, as is my right lick the other cats eat owner's food attack dog, run away and pretend to be victim. Scratch at the door then walk away paw at beetle and eat it before it gets away so eat from dog's food or burrow under covers, and meow but jump around on couch, meow constantly until given food, , or lick arm hair. Brown cats with pink ears vommit food and eat it again poop on grasses drink water out of the faucet play time jump around on couch, meow constantly until given food, .</p>
-			<!--unordered list-->
-			<ul>
-				<li>item 1</li>
-				<li>item 2</li>
-				<li>item 3</li>
-			</ul>
-			<!--ordered list-->
-			<ol>
-				<li>item 1</li>
-				<li>item 2</li>
-				<li>item 3</li>
-			</ol>
-			<!-- insert google.com-->
-			<div>
-				<p>Go to <a href="https://google.com" target="_blank">Google.com</a></p>
-			</div>
-			<!--insert cartoon image-->
-			<img src="images/cartoon.jpeg" alt="cartoon"/>
-			<!--insert table-->
-			<table>
-				<tr>
-					<th>
-						Table Column 1
-					</th>
-					<th>
-						Table Column 2
-					</th>
-				</tr>
-				<tr>
-					<td>
-						Table Cell 1
-					</td>
-					<td>
-						Table Cell 2
-					</td>
-				</tr>
-			</table>
-		</main>
-		<footer>
-			&copy; 2016 <!--&copy is the copyright symbol-->
-		</footer>
+
 	</body>
 </html>
