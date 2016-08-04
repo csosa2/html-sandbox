d9e9150ae83178b994b9cf1f4f8fa450961e9bad
Christina Sosa <csosa2106@gmail.com>
2016-08-04 13:16:25 -0600

    added navbar

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <!DOCTYPE html>
 <html lang="en">
 	<head>
-		<meta charset="utf-8"/>
-		<meta http-equiv="x-ua-compatible" content="IE=edge"/>
-		<meta name="viewport" content="width=device-width, initial-scale=1"/>
+		<meta charset="UTF-8" />
+		<meta http-equiv="X-UA-Compatible" content="IE=edge"/>
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
 
 		<!-- Latest compiled and minified CSS -->
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
@@ -11,31 +11,114 @@
 		<!-- Optional theme -->
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
 
-		<!--ADD OTHER CSS FILES HERE-->
+		<!-- ADD OTHER CSS FILES HERE -->
 
-
-		<!--ADD YOUR CUSTOM CSS HERE-->
+		<!-- ADD YOUR CUSTOM CSS HERE -->
 		<link rel="stylesheet" href="css/style.css" type="text/css" />
 
 		<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-		<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
 		<!--[if lt IE 9]>
-		<script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
-		<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+		<script type="text/javascript" src="//oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+		<script type="text/javascript" src="//oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
 		<![endif]-->
 
-		<!--JQuery-->
-		<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/1.9.1/jquery.min.js" type="text/css"></script>
+		<!-- jQuery -->
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/1.9.1/jquery.min.js" type="text/javascript"></script>
 
 		<!-- Latest compiled and minified JavaScript -->
 		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
 
-		<!--ADD YOU CUSTOM JS FILES HERE-->
+		<!-- ADD YOUR CUSTOM JS FILES HERE -->
 		<script src="js/custom.js" type="text/javascript"></script>
 
-		<title>My Bootstrap Page</title>
+		<title>mY bOOTSTRAP pAGE</title>
 	</head>
-	<body>
-		HAI! :D
+	<body class="sfooter">
+
+		<!-- begin sticky footer content -->
+		<div class="sfooter-content">
+
+			<!-- begin header -->
+			<header>
+				<div class="container">
+					<div class="row">
+						<div class="col-md-12">
+
+							<!-- begin navbar -->
+							<nav class="navbar navbar-default">
+								<!-- Brand and toggle get grouped for better mobile display -->
+								<div class="navbar-header">
+									<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+										<span class="sr-only">Toggle navigation</span>
+										<span class="icon-bar"></span>
+										<span class="icon-bar"></span>
+										<span class="icon-bar"></span>
+									</button>
+									<a class="navbar-brand" href="#">My Bootstrap Page</a>
+								</div>
+
+								<!-- Collect the nav links, forms, and other content for toggling -->
+								<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+									<ul class="nav navbar-nav navbar-right">
+										<li><a href="#">Home</a></li>
+										<li><a href="about-me">About Me</a></li>
+										<li class="dropdown">
+											<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Moar Links <span class="caret"></span></a>
+											<ul class="dropdown-menu">
+												<li><a href="#">Action</a></li>
+												<li><a href="#">Another action</a></li>
+												<li><a href="#">Something else here</a></li>
+												<li role="separator" class="divider"></li>
+												<li><a href="#">Separated link</a></li>
+											</ul>
+										</li>
+									</ul>
+								</div><!-- /.navbar-collapse -->
+							</nav>
+
+						</div>
+					</div>
+				</div>
+			</header>
+
+			<!-- begin main content -->
+			<main>
+				<div class="container">
+					<div class="row">
+						<div class="col-md-3">
+							side panel
+						</div>
+						<div class="col-md-9">
+							content panel
+						</div>
+					</div>
+					<div class="row">
+						<div class="col-md-4">
+							column one
+						</div>
+						<div class="col-md-4">
+							column two
+						</div>
+						<div class="col-md-4">
+							column 3
+						</div>
+					</div>
+				</div>
+			</main>
+		</div>
+
+		<!-- begin footer here -->
+		<footer>
+			<div class="container">
+				<div class="row">
+					<div class="col-md-6">
+						footer column one
+					</div>
+					<div class="col-md-6">
+						footer column two
+					</div>
+				</div>
+			</div>
+		</footer>
 	</body>
 </html>
